9cc69ca33b09682c94d68f6245b1d5e798c88d71
jaffa Isaac <jaffaisaac4@gmail.com>
2023-02-25 17:32:20 +0300

    Add files via upload

---
+++ min.js
@@ -0,0 +1,17 @@
+// Class Declaration 
+
+class Circle{
+    constructor(){
+        this.radius =3.14;
+        }
+    }
+
+//class Expressions 
+
+const Circle = class{
+    constructor(){
+        this.radius =3.14;
+    }
+}
+
+// this above code expressions are all the same 
